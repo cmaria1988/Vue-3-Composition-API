0cb60d806fd2ac491312bfd47029aa660168c46e
Maria <claudmaria1988@gmail.com>
2024-05-15 18:50:08 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,18 @@
-<script>
-import {computed, ref, reactive,toRef, toRefs, watch} from 'vue'
+<script setup>
+import CartItem from "./components/CartItem.vue";
+import useCart from "./useCart";
 
-export default{
-  setup(){
-    const item = reactive({
-      name: "Product 1",
-      price: 10,
-      quantity: 1
-    })
-
-    const increment = () =>item.quantity++
-
-    const decrement = () =>item.quantity--
-
-    const swapProduct = () => {
-      item.name = "Product A"
-      item.price = 30
-    }
-
-    const total = computed(()=>item.price * item.quantity)
-
-    const {name, price, quantity} = toRefs(item)
-
-    watch(total, (newValue, oldValue)=>{
-      console.log('newVlaue', newValue)
-      console.log('oldValue', oldValue)
-    })
-
-    return{
-      quantity,
-      increment,
-      decrement,
-      name,
-      price,
-      total,
-      swapProduct
-    }
-  }
-}
+const {items, addItem, removeItem} = useCart()
+addItem({
+        id:1,
+        name: "Product X",
+        price: 20,
+        quantity:1
+      })
 </script>
 
 <template>
-  <h1>{{ name }}:{{ price }}</h1>
-  <button @click="swapProduct">Swap product</button>
-  <button @click="price++">Increment price</button>
-
-  <h2>{{ quantity }}</h2>
-  <button @click="increment">+</button>
-  <button @click="decrement">-</button>
-
-  <h3>Total: {{ total }}</h3>
+  <CartItem v-for="item in items" :cart-item="item" :key="item.id" @remove="removeItem"/>
 </template>
 
 <style scoped>
---
+++ src/components/CartItem.vue
@@ -0,0 +1,40 @@
+<script setup>
+import {computed, reactive, toRefs, onMounted, onUpdated, onUnmounted} from 'vue'
+
+const props = defineProps({
+    cartItem:{
+            type: Object,
+            required: true
+        }
+})
+
+const emit = defineEmits(['remove'])
+
+const item = reactive(props.cartItem)
+
+const increment = () =>item.quantity++
+
+const decrement = () =>item.quantity--
+
+const total = computed(()=>item.price * item.quantity)
+
+const {name, price, quantity} = toRefs(item)
+
+const remove = () => emit('remove',item)
+
+</script>
+
+<template>
+  <h1>{{ name }}:{{ price }}:{{ quantity }}</h1>
+
+  <h2>{{ quantity }}</h2>
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
+    <br />
+  <button @click="remove">Remove</button>
+
+  <h3>Total: {{ total }}</h3>
+</template>
+
+<style scoped>
+</style>
