3f7012d548a68d4273b7216b9a826c321f7356d9
Maria <claudmaria1988@gmail.com>
2024-05-15 14:06:42 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script>
+import {computed, ref, reactive,toRef, toRefs, watch} from 'vue'
+
+export default{
+  setup(){
+    const item = reactive({
+      name: "Product 1",
+      price: 10,
+      quantity: 1
+    })
+
+    const increment = () =>item.quantity++
+
+    const decrement = () =>item.quantity--
+
+    const swapProduct = () => {
+      item.name = "Product A"
+      item.price = 30
+    }
+
+    const total = computed(()=>item.price * item.quantity)
+
+    const {name, price, quantity} = toRefs(item)
+
+    watch(total, (newValue, oldValue)=>{
+      console.log('newVlaue', newValue)
+      console.log('oldValue', oldValue)
+    })
+
+    return{
+      quantity,
+      increment,
+      decrement,
+      name,
+      price,
+      total,
+      swapProduct
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ name }}:{{ price }}</h1>
+  <button @click="swapProduct">Swap product</button>
+  <button @click="price++">Increment price</button>
+
+  <h2>{{ quantity }}</h2>
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
+
+  <h3>Total: {{ total }}</h3>
+</template>
+
+<style scoped>
+</style>
